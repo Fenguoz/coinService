a3ac652b9c28b2ec9059df59dc79d19563878285
Fenguoz <243944672@qq.com>
2021-04-28 18:04:13 +0800

    优化归集时间

--- config/autoload/crontab.php
+++ config/autoload/crontab.php
@@ -7,6 +7,6 @@ return [
     // 通过配置文件定义的定时任务
     'crontab' => [
         // (new Crontab())->setName('ETHFundsCollection')->setRule('* 0,8,16 * * *')->setCallback([App\Task\ETHFundsCollection::class, 'execute'])->setMemo('ETH归集'),
-        (new Crontab())->setName('TRONFundsCollection')->setRule('* 4,12,20 * * *')->setCallback([App\Task\TRONFundsCollection::class, 'execute'])->setMemo('TRON归集'),
+        (new Crontab())->setName('TRONFundsCollection')->setRule('0 0,4,8,12,16,20 * * *')->setCallback([App\Task\TRONFundsCollection::class, 'execute'])->setMemo('TRON归集'),
     ],
 ];
